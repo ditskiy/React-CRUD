289ec88aa0eb7695e99f9ba852842ddbd2f67ae5
DimaWork <dditskiy@gmail.com>
2023-03-21 16:01:06 +0200

    add item form

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -1,11 +1,11 @@
 import "./app-info.css";
 
-const AppInfo = () => {
+const AppInfo = ({employers, incrised}) => {
     return (
         <div className="app-info">
             <h1>Учет сотрудников в компании ІВ</h1>
-            <h2>Общее число сотрудников: </h2>
-            <h2>Премию получат: </h2>
+            <h2>Общее число сотрудников: {employers}</h2>
+            <h2>Премию получат: {incrised}</h2>
         </div>
     );
 }
--- src/components/employers-list/employers-list.js
+++ src/components/employers-list/employers-list.js
@@ -2,12 +2,17 @@ import EmployersListItem from "../employers-list-item/employers-list-item";
 
 import "./employers-list.css"
 
-const EmployersList = ({data}) => {
+const EmployersList = ({data, onDelite, onToggleIncrease, onToggleRise}) => {
 
     const elements = data.map(item => {
         const {id, ...itemProps} = item;
         return (
-            <EmployersListItem key={id} {...itemProps}/>
+            <EmployersListItem 
+                key={id} 
+                {...itemProps}
+                onDelite={() => onDelite(id)}
+                onToggleIncrease={() => onToggleIncrease(id)}
+                onToggleRise={() => onToggleRise(id)}/>
         )
     })
 
